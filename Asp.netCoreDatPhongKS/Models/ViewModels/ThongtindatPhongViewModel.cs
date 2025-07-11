32cc1e78fe88218adf00bdeecef12bdc06f667ee
DinhNguyen2011 <dinhn6666@gmail.com>
2025-07-11 11:01:33 +0700

    new

--- Asp.netCoreDatPhongKS/Models/ViewModels/ThongtindatPhongViewModel.cs
+++ Asp.netCoreDatPhongKS/Models/ViewModels/ThongtindatPhongViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Asp.netCoreDatPhongKS.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.netCoreDatPhongKS.Models.ViewModels
 {
     public class ThongtindatPhongViewModel
     {
@@ -7,13 +9,31 @@
         public DateTime Checkout { get; set; }
         public int SoDem => (Checkout - Checkin).Days;
 
-        // Thông tin khách hàng
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        [Display(Name = "Họ và tên")]
         public string HoTen { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải bắt đầu bằng 0 và có đúng 10 chữ số")]
+        [Display(Name = "Số điện thoại")]
         public string SoDienThoai { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ")]
+        [Display(Name = "Địa chỉ")]
         public string DiaChi { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số CCCD")]
+        [RegularExpression(@"^\d{12}$", ErrorMessage = "CCCD phải có đúng 12 chữ số")]
+        [Display(Name = "Số CCCD")]
         public string Cccd { get; set; }
-        public string GhiChu { get; set; }
+
+        [Display(Name = "Ghi chú")]
+        public string? GhiChu { get; set; }
         // Thông tin phòng
         public Phong? Phong { get; set; }
     }
--- Asp.netCoreDatPhongKS/obj/Debug/net6.0/Asp.netCoreDatPhongKS.AssemblyInfo.cs
+++ Asp.netCoreDatPhongKS/obj/Debug/net6.0/Asp.netCoreDatPhongKS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.netCoreDatPhongKS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278236e4946d26db4e28d0e5720eae4bb5bd9f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5c5f28d7562766d074f6845930dca2d289adb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.netCoreDatPhongKS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.netCoreDatPhongKS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
